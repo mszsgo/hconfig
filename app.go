f4b2b73c6e6f2e19fdcaf159f93d48820381e26f
zengs <zengs@unionlive.com>
2020-02-15 05:52:26 +0800

    1.0 fix app

--- app.go
+++ app.go
@@ -1,13 +1,62 @@
 package hconfig
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"runtime"
+)
+
 // 应用配置
 type App struct {
 	Name        string `json:"name"`
 	Version     string `json:"version"`
 	Description string `json:"description"`
 
-	Host int64 `json:"host"`
-	Port int64 `json:"port"`
+	Host string `json:"host"`
+	Port int    `json:"port"`
 
 	LogLevel string `json:"logLevel"`
+}
+
+// 启动服务
+func (app *App) Start(preFunc func(app *App)) {
+	log.Println("******************************************************************************************")
+	defer printStack()
+	app.Args()
+	if preFunc != nil {
+		preFunc(app)
+	}
+	app.ListenAndServe()
+}
+
+func (app *App) Args() {
+	var (
+		// 服务名与端口号
+		name string
+		port int
+	)
+	flag.StringVar(&name, "name", app.Name, fmt.Sprintf("Set Application name. Default '%s'", app.Name))
+	flag.IntVar(&port, "port", app.Port, fmt.Sprintf("Set Port. Default is %d", app.Port))
+
+	flag.Parse()
+
+	app.Name = name
+	app.Port = port
+}
+
+func printStack() {
+	var buf [4096]byte
+	n := runtime.Stack(buf[:], false)
+	log.Printf("==> %s\n", string(buf[:n]))
+
+	if err := recover(); err != nil {
+		log.Fatalf("** Main Fatalf-> %s", err)
+	}
+}
+
+func (app *App) ListenAndServe() {
+	log.Printf("MicroService: %s  ListenAndServe %s:%d   Start server http://127.0.0.1:%d", app.Name, app.Host, app.Port, app.Port)
+	panic(http.ListenAndServe(fmt.Sprintf("%s:%d", app.Host, app.Port), nil))
 }
--- mongo.go
+++ mongo.go
@@ -3,3 +3,11 @@ package hconfig
 type MongoConfig struct {
 	ConnectionString string `json:"connectionString"`
 }
+
+// hmgdb接收的参数为mongodb连接字符串
+func GetMongoConnectionString() string {
+	// 获取DB连接
+	var mc *MongoConfig
+	NowConfig(nil, "mongo", &mc)
+	return mc.ConnectionString
+}
